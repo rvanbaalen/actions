384bc19104aada71545a6f72d18a24b24d00ce62
Robin van Baalen <robin@cattlea.com>
2020-05-16 15:31:18 -0400

    Update main.js

--- docker-gpr/main.js
+++ docker-gpr/main.js
@@ -7,8 +7,8 @@ async function run() {
   const username = process.env.GITHUB_ACTOR;
   const imageName = core.getInput("image-name").toLowerCase();
   const githubRepo = process.env.GITHUB_REPOSITORY.toLowerCase();
-  const tag = process.env.GITHUB_SHA;
-  const fullImageReference = `docker.pkg.github.com/${githubRepo}/${imageName}:${tag}`;
+  const tag = process.env.GITHUB_SHA.substr(0,7);
+  const fullImageReference = (tag) => `docker.pkg.github.com/${githubRepo}/${imageName}:${tag}`;
   const makeLatest = !!core.getInput('make-latest') || false;
   
   try {
@@ -20,28 +20,22 @@ async function run() {
   }
   try {
     await exec.exec(
-      `docker build -t ${fullImageReference} ${dockerfileLocation}`
+      `docker build -t ${fullImageReference(tag)} ${dockerfileLocation}`
+    );
+    await exec.exec(
+      `docker build -t ${fullImageReference('latest')} ${dockerfileLocation}`
     );
   } catch (err) {
     core.setFailed(`action failed with error: ${err}`);
   }
   
-  if (makeLatest) {
-    try {
-      await exec.exec(
-        `docker build -t docker.pkg.github.com/${githubRepo}/${imageName}:latest ${dockerfileLocation}`
-      );
-    } catch (err) {
-      core.setFailed(`action failed with error: ${err}`);
-    }
-  }
-  
   try {
-    await exec.exec(`docker push ${fullImageReference}`);
+    await exec.exec(`docker push ${fullImageReference(tag)}`);
+    await exec.exec(`docker push ${fullImageReference('latest')}`);
   } catch (err) {
     core.setFailed(`Review the logs above, most likely you are using a package name associated with a different repository.  Rename your Image to fix. https://help.github.com/en/github/managing-packages-with-github-packages/about-github-packages#managing-packages for more information`);
   }
-  core.setOutput("imageUrl", fullImageReference);
+  core.setOutput("imageUrl", fullImageReference(tag));
 }
 
 run();
